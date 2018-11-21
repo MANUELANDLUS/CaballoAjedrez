bc97ba81c9bab82afadce6c7be3f48ccc38fa06b
MANUEL SERNA GARCIA <MANUEL81SG@GMAIL.COM>
2018-11-21 23:26:37 +0100

    Creo los métodos get y set para los atributos.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -3,4 +3,28 @@ package org.iesalandalus.programacion.caballoajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+
+	public void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("El valor de la fila debe ser del 1 al 8.");
+		} else {
+			this.fila = fila;
+		}
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	public void setColumna(char columna) {
+		if (columna >= 'a' && columna <= 'h') {
+			this.columna = columna;
+		} else {
+			throw new IllegalArgumentException("El valor de la columna debe ser un caracter de la \"a\" a la \"h\".");
+		}
+	}
+
+	public char getColumna() {
+		return columna;
+	}
 }
