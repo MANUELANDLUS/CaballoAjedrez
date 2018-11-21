06ed479cd065c8b0a3e834290761550b9fcdf8de
MANUEL SERNA GARCIA <MANUEL81SG@GMAIL.COM>
2018-11-22 00:26:33 +0100

    Ejercicio 12
    Creo los métodos get y set para cada atributo con la visibilidad
    adecuada.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -6,6 +6,34 @@ public class Caballo {
 	private Color color;
 	private Posicion posicion;
 
-	//private static final String ERROR_MOVIMIENTO = "Movimiento no permitido: ";
+	// private static final String ERROR_MOVIMIENTO = "Movimiento no permitido: ";
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		if (color == null) {
+			throw new IllegalArgumentException("El color no puede ser nulo.");
+		}
+		if (color == Color.BLANCO) {
+			posicion = new Posicion(1, 'b');
+		}
+		if (color == Color.NEGRO) {
+			posicion = new Posicion(8, 'b');
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new IllegalArgumentException("La posición no puede ser nula.");
+		}
+		this.posicion = new Posicion(posicion);
+	}
 
 }
