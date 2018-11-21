e3a91f61fe7b3bd242f9468d70390c86df53ffe1
MANUEL SERNA GARCIA <MANUEL81SG@GMAIL.COM>
2018-11-22 00:22:26 +0100

    Ejercicio 11
    Creo la clase Caballo, dentro del paquete adecuado, cuyos atributos
    serán un color (del tipo enumerado Color) y posicion (de la clase
    Posicion), con la visibilidad adecuada.

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.caballoajedrez;
+
+import javax.naming.OperationNotSupportedException;
+
+public class Caballo {
+	private Color color;
+	private Posicion posicion;
+
+	//private static final String ERROR_MOVIMIENTO = "Movimiento no permitido: ";
+
+}
